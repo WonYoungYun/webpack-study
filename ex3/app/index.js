cc91860ba29393f06a87b0759164181acae946ec
WonYoungYun <asmar20288@naver.com>
2018-10-16 20:11:23 +0900

    resolve

---
+++ ex3/app/index.js
@@ -0,0 +1,10 @@
+// var $ = require('jquery')
+// console.log("loaded jQuery version is " + $.fn.jquery);
+
+// alias
+// import $ from 'Vendor/jquery-2.2.4.min.js'
+// console.log("loaded jQuery version is " + $.fn.jquery);
+
+
+//ProvidePlugin
+console.log("loaded jQuery version is " + $.fn.jquery);
